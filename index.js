3cf2ea43a65f53c7f8272b26caaf4c28e8de030a
bb4dm <babych.dmtr@gmail.com>
2026-08-20 22:24:16 +0300

    init commit

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import data from "./data.json" with { type: "json" };
 
 const score = document.getElementById("score");
+
 const avgScore =
   data.reduce((acc, value) => {
     return acc + value.score;
   }, 0) / data.length;
 
-score.textContent = avgScore;
+score.textContent = Math.round(avgScore);
+
+const results = data.map((el) => {
+  return `<li>
+      <img src=${el.icon} alt=${el.category} />
+      <h3>${el.category}</h3>
+      <p>
+        <span>alt=${el.score}</span> / 100
+      </p>
+    </li>`;
+});
